228e47158e0ff2fe707e0d0c514c1332cc48a62c
Lam <github@Lam.pl>
2020-04-20 20:34:41 +0200

    Revert "[mgtv] Extract format_note (#21881)"
    This reverts commit e1fcfc05e705f16315c8cafe8afd437df33b7614.

--- youtube_dl/extractor/mgtv.py
+++ youtube_dl/extractor/mgtv.py
@@ -82,7 +82,6 @@ class MGTVIE(InfoExtractor):
                 'http_headers': {
                     'Referer': url,
                 },
-                'format_note': stream.get('name'),
             })
         self._sort_formats(formats)
 
